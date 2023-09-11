1024ce893c175c459571784040bf06877f70eefa
Louise <lkosborne@live.com>
2023-09-11 09:16:26 +0100

    removed console logs for troubleshooting

--- src/controllers/favourites.js
+++ src/controllers/favourites.js
@@ -4,12 +4,9 @@ const createFavourite = async (req, res) => {
   const { pose, category } = req.body;
 
   try {
-    //console.log('Attempting to create a favorite pose...');
     const { rows: [favourite] } = await db.query('INSERT INTO Favourites (pose, category) VALUES ($1, $2) RETURNING *', [pose, category]);
-    //console.log('Favorite pose created successfully:', favourite);
     res.status(201).json(favourite);
   } catch (err) {
-    //console.error('Error creating favorite pose:', err);
     res.status(500).json(err.message);
   }
 };
