d460a27d3ce9c2f3d80818bc52f949acbf9bc8be
Louise <lkosborne@live.com>
2023-09-13 19:56:21 +0100

    removed api code

--- api/api.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-async function fetchDataFromAPI() {
-  const response = await axios.get('https://yoga-api-nzy4.onrender.com/v1/poses');
-  return response.data; // Assuming the API response is an array of poses
-}
-
-module.exports = { fetchDataFromAPI };
--- migrations/001-create-favourites-table.sql
+++ migrations/001-create-favourites-table.sql
@@ -1,14 +1,9 @@
 CREATE TABLE Favourites (
   id VARCHAR(100) PRIMARY KEY,
-  english_name VARCHAR(255) NOT NULL,
-  sanskrit_name VARCHAR(255) NOT NULL,
+  pose VARCHAR(255) NOT NULL,
   sanskrit_name_adapted VARCHAR(255) NOT NULL,
-  translation_name VARCHAR(255) NOT NULL,
   pose_description VARCHAR(512) NOT NULL,
   pose_benefits VARCHAR(512) NOT NULL,
-  url_svg VARCHAR(255) NOT NULL,
   url_png VARCHAR(255) NOT NULL,
-  url_svg_alt VARCHAR(255) NOT NULL,
-  pose VARCHAR(255) NOT NULL,
   category VARCHAR(255) NOT NULL
   );
