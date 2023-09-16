12b714f91cc2d1befa133552c8761e30e57c1216
Louise <lkosborne@live.com>
2023-09-16 20:39:12 +0100

    reduced poses to five categories

---
+++ migrations/001-create-poses-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Poses (
+  id VARCHAR(100) PRIMARY KEY,
+  pose VARCHAR(255) NOT NULL,
+  sanskrit_name VARCHAR(255) NOT NULL,
+  pose_description VARCHAR(512) NOT NULL,
+  pose_benefits VARCHAR(512) NOT NULL,
+  url_png VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL
+  );
--- migrations/002-create-favourites-table.sql
+++ migrations/002-create-favourites-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Favourites (
   id VARCHAR(100) PRIMARY KEY,
   pose VARCHAR(255) NOT NULL,
-  sanskrit_name_adapted VARCHAR(255) NOT NULL,
+  sanskrit_name VARCHAR(255) NOT NULL,
   pose_description VARCHAR(512) NOT NULL,
   pose_benefits VARCHAR(512) NOT NULL,
   url_png VARCHAR(255) NOT NULL,
