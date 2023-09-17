914d9630aea3cb27c8cfb96825d5543c3d204738
Louise <lkosborne@live.com>
2023-09-17 15:05:59 +0100

    added sync users to take user data from firebase and add to database

---
+++ sync-users.js
@@ -0,0 +1,43 @@
+const { Client } = require('pg');
+const admin = require('./firebase');
+
+// Database connection configuration
+const dbConfig = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'yoga_app_dev',
+  password: 'password',
+  port: 5432,
+};
+
+const client = new Client(dbConfig);
+
+async function syncUsersToDatabase() {
+  try {
+    // Connect to the PostgreSQL database
+    await client.connect();
+
+    // Fetch user data from Firebase
+    const users = await admin.auth().listUsers();
+
+    // Insert users into the Users table
+    const insertQuery = 'INSERT INTO Users (firebase_uid, display_name, email) VALUES ($1, $2, $3)';
+
+    const insertPromises = users.users.map((user) => {
+      const values = [user.uid, user.displayName || null, user.email];
+      return client.query(insertQuery, values);
+    });
+
+    await Promise.all(insertPromises);
+
+    console.log('User synchronization completed successfully.');
+  } catch (error) {
+    console.error('Error synchronizing users:', error);
+  } finally {
+    // Close the database connection
+    await client.end();
+  }
+}
+
+// Call the function to start user synchronization
+syncUsersToDatabase();
