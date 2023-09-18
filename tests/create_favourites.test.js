ecb6659d64cc99be2282eb781b19c713a6221c2b
Louise <lkosborne@live.com>
2023-09-18 09:56:10 +0100

    updated create_favourites test

--- tests/create_favourites.test.js
+++ tests/create_favourites.test.js
@@ -6,10 +6,15 @@ const app = require('../src/app');
 describe('create favourite pose', () => {
   describe('/favourites', () => {
     describe('POST', () => {
-      it('creates a new favourite pose in the database', async () => {
+      it('creates a new favourite pose in the favourites table', async () => {
         const requestBody = {
-          pose: 'Example Pose',
-          category: 'Backbend',
+          pose_id: '1',
+          pose_name: 'Boat',
+          sanskrit_name: 'Navasana',
+          pose_description: 'Description',
+          url_png: 'a url',
+          category: 'Core',
+          level: 'Intermediate',
         };
 
         const response = await request(app)
@@ -17,8 +22,13 @@ describe('create favourite pose', () => {
           .send(requestBody);
 
         expect(response.status).to.equal(201);
-        expect(response.body.pose).to.equal('Example Pose');
-        expect(response.body.category).to.equal('Backbend');
+        expect(response.body.pose_id).to.equal('1');
+        expect(response.body.pose_name).to.equal('Boat');
+        expect(response.body.sanskrit_name).to.equal('Navasana');
+        expect(response.body.pose_description).to.equal('Description');
+        expect(response.body.url_png).to.equal('a url');
+        expect(response.body.category).to.equal('Core');
+        expect(response.body.level).to.equal('Intermediate');
       });
     });
   });
